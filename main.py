7a2a406c043394d46778668dede3b9a3343a6757
Robbemans <Robertblaak@outlook.com>
2026-02-03 19:57:06 +0100

    add response and client

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from llm!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
