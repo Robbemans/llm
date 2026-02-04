281db581bb99ab2b2616dd2f7ac4fe161698d600
Robbemans <robertblaak@outlook.com>
2026-02-04 09:08:16 +0100

    fixed __name__, add token counts

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def main():
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    usage = response.usage_metadata
+    prompt_token_count = usage.prompt_token_count
+    candidates_token_count = usage.candidates_token_count
+    if usage is not None:
+        print(f"Prompt tokens: {prompt_token_count}\nResponse tokens: {candidates_token_count}")
+        print(response.text)
+    else:
+        raise RuntimeError("usage_metadata is None!")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
